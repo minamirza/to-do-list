89e871f25dc257796a513bd67e2481c43cbc5f4f
minamirza <minamirzeva@gmail.com>
2025-05-16 23:27:48 +0400

    create

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='Header'>
+        <nav  className='HeaderComp'>
+            <ul className='HeaderCompDet'>
+                <li>
+                    Home
+                </li>
+                <li>
+                    Reminder
+                </li>
+                <li>
+                    Contact
+                </li>
+            </ul>
+        </nav>
+    </div>
   )
 }
 
